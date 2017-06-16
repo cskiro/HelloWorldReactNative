34b8f1dc9bc045ecab4c8ab98de2320089416480
Connor Skiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-06-16 11:32:12 -0400

    Creating a simple Movies app
    Fetches 25 movies that are in theaters and displays them in a ListView

--- index.android.js
+++ index.android.js
@@ -4,43 +4,128 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component,
+} from 'react';
 import {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-export default class HelloWorldReactNative extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-            Hello World!
-        </Text>
-      </View>
-    );
-  }
-}
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class HelloWorldReactNative extends Component {
+     /** Adding initial state to determine if data can be loaded or not */
+     constructor(props) {
+        super(props);
+        this.state = {
+          dataSource: new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+          }),
+          loaded: false,
+        };
+      }
+
+      /**
+       * Sending of the request after the component has finished loading. This will only get called once.
+       */
+      componentDidMount() {
+        this.fetchData();
+      }
+
+      /** Responsible for handling the fetching of the data */
+      fetchData() {
+        fetch(REQUEST_URL)
+          .then((response) => response.json())
+          .then((responseData) => {
+            /** Setting the data when the response comes back */
+            this.setState({
+              dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+              loaded: true,
+            });
+          })
+          .done();
+      }
+
+        /**
+         * Renders a ListView of movies once data comes back
+         */
+      render() {
+        if (!this.state.loaded) {
+          return this.renderLoadingView();
+        }
+
+        return (
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderMovie}
+            style={styles.listView}
+          />
+        );
+      }
+
+      renderLoadingView() {
+        return (
+          <View style={styles.container}>
+            <Text>
+              Loading movies...
+            </Text>
+          </View>
+        );
+      }
+
+      renderMovie(movie) {
+        return (
+          <View style={styles.container}>
+            <Image
+              source={{uri: movie.posters.thumbnail}}
+              style={styles.thumbnail}
+            />
+            <View style={styles.rightContainer}>
+              <Text style={styles.title}>{movie.title}</Text>
+              <Text style={styles.year}>{movie.year}</Text>
+            </View>
+          </View>
+        );
+      }
+    }
+
+    var styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        /** Lays out children horizontally within the container */
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
+      rightContainer: {
+        flex: 1,
+      },
+      title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+      },
+      year: {
+        textAlign: 'center',
+      },
+      thumbnail: {
+        width: 53,
+        height: 81,
+      },
+      listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+      },
+    });
 
 AppRegistry.registerComponent('HelloWorldReactNative', () => HelloWorldReactNative);
